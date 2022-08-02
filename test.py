fd526863a433ae775884a1473d6f130dc57cf9db
Yuuuugo <hugogabrielidis@gmail.com>
2022-08-03 01:26:27 +0200

    change of test file

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from config import Config
 import numpy as np
 from helper_functions import calculate_results
 import pickle
+import numpy as np
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="FOOD101")
@@ -27,8 +28,16 @@ if __name__ == "__main__":
     config = Config()
 
     # Extract X and y from test tf.data.Dataset
-    X_test = np.concatenate([x for x, _ in test_ds], axis=0)
-    y_test = np.concatenate([y for _, y in test_ds], axis=0)
+
+    X_test = []
+    y_test = []
+
+    for img, label in test_ds.unbatch():
+        X_test.append(img)
+        y_test.append(label)
+
+    X_test = np.array(X_test)
+    y_test = np.array(y_test)
 
     # When not running on GPU only take a subsample of the set
     if not eval(args.GPU):
@@ -39,8 +48,6 @@ if __name__ == "__main__":
     y_pred = model.predict(X_test, verbose=1)
     y_pred = [np.argmax(x) for x in y_pred]
     model.evaluate(X_test, y_test)
-    print(y_test)
-    print(y_pred)
     # Will returns a dictionnary with the following metrics : Accuracy, Precision, Recall, F1-score
     results = calculate_results(y_test, y_pred)
     print(results)
